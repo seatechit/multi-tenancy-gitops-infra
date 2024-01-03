60de8fbe94ca3e562fcd37508c531afad30fabe6
lee <anhldpro@yahoo.com>
2024-01-02 21:59:30 -0500

    fx

--- consolelink/get-gitops-cntk-consollink.sh
+++ consolelink/get-gitops-cntk-consollink.sh
@@ -17,3 +17,64 @@ GIT_BASEURL=${GIT_BASEURL:-https://github.com}
 GIT_BASEURL=${GIT_BASEURL} \
 sh > git-org-consolelink.yaml
 echo ${GIT_BASEURL}/${GIT_ORG}
+
+### CP4I Dashboard
+#ACE
+CP_CONSOLE_URL=$(oc get route -n ibm-common-services | grep cp-console | awk '{print "https://"$2}')
+CPD_URL=$(oc get route -n tools | grep cpd | awk '{print "https://"$2}')
+INTEGRATION_NAV_URL=$(oc get route -n tools | grep integration-navigator-pn | awk '{print "https://"$2}')
+
+
+NAME="cp-console"
+SECTION="ACE"
+URL=${CP_CONSOLE_URL}
+TEXT="CP Console"
+( echo "cat <<EOF" ; cat consolelink.yaml_template;) | \
+NAME=${NAME} SECTION=${SECTION} URL=${URL} TEXT=${TEXT} \
+sh > cp-console-link.yaml
+
+NAME="cpd-console"
+SECTION="ACE"
+URL=${CPD_URL}
+TEXT="CPD Console"
+( echo "cat <<EOF" ; cat consolelink.yaml_template;) | \
+NAME=${NAME} SECTION=${SECTION} URL=${URL} TEXT=${TEXT} \
+sh > cpd-console-link.yaml
+
+NAME="integration-nav"
+SECTION="ACE"
+URL=${INTEGRATION_NAV_URL}
+TEXT="Integration Navigator"
+( echo "cat <<EOF" ; cat consolelink.yaml_template;) | \
+NAME=${NAME} SECTION=${SECTION} URL=${URL} TEXT=${TEXT} \
+sh > integration-nav-link.yaml
+
+#APIC
+APIC_CLOUD_URL=$(oc get route -n tools | grep admin | awk '{print "https://"$2}')
+APIC_MANAGER_URL=$(oc get route -n tools | grep api-manager | awk '{print "https://"$2}')
+APIC_PORTAL_URL=$(oc get route -n tools | grep portal-web | awk '{print "https://"$2}')
+
+
+NAME="apic-cloud"
+SECTION="APIC"
+URL=${APIC_CLOUD_URL}
+TEXT="APIC Cloud Manager"
+( echo "cat <<EOF" ; cat consolelink.yaml_template;) | \
+NAME=${NAME} SECTION=${SECTION} URL=${URL} TEXT=${TEXT} \
+sh > apic-cloud-link.yaml
+
+NAME="apic-api-manager"
+SECTION="APIC"
+URL=${APIC_MANAGER_URL}
+TEXT="APIC API Manager"
+( echo "cat <<EOF" ; cat consolelink.yaml_template;) | \
+NAME=${NAME} SECTION=${SECTION} URL=${URL} TEXT=${TEXT} \
+sh > apic-api-manager-link.yaml
+
+NAME="apic-portal-manager"
+SECTION="APIC"
+URL=${APIC_PORTAL_URL}
+TEXT="APIC Portal Manager"
+( echo "cat <<EOF" ; cat consolelink.yaml_template;) | \
+NAME=${NAME} SECTION=${SECTION} URL=${URL} TEXT=${TEXT} \
+sh > apic-portal-manager-link.yaml
